8da1215162b887ce1420243988a9772dee074e60
Matt Antonelli <contact@raelys.com>
2026-09-08 22:43:37 +0000

    Add beastmaster and blue mage source types
    Migrates the BLU/BST source lists to the regular one so we can have
    pretty icons.

--- app/helpers/spells_helper.rb
+++ app/helpers/spells_helper.rb
@@ -3,32 +3,6 @@ module SpellsHelper
     (1..5).to_a.reverse.map { |x| ["\u2605" * x, x] }
   end
 
-  def spell_sources(spell, limit: 3)
-    count = spell.sources.size
-    sources = spell.sources.first(limit).map do |source|
-      text = source.text
-
-      if text.match?(' / ')
-        text.split(' / ')
-      else
-        # Handle splits for sources with no Enemy details
-        ["&nbsp;", text]
-      end
-    end
-
-    if count == 0
-      # Handle spells with no recorded sources
-      sources = [["&nbsp;", "&nbsp;"]]
-    elsif count > limit
-      additional = count - limit + 1
-      sources.pop
-      sources << ["&nbsp;", link_to("#{additional} more #{'source'.pluralize(additional)}",
-                                    spell_path(spell), class: 'font-italic')]
-    end
-
-    sources.transpose.map { |details| details.join('<br>').html_safe }
-  end
-
   def spell_aspect(spell)
     spell.type.name_en == 'Physical' ? t('spells.damage') : t('spells.aspect')
   end
