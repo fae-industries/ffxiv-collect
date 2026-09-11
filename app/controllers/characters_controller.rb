c0f113c1a755c4def544fda1e492b37ff7376193
Matt Antonelli <contact@raelys.com>
2026-09-11 00:40:01 +0000

    Update character profile collection order

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,8 +8,8 @@ class CharactersController < ApplicationController
   before_action :set_verification_code, only: [:verify, :validate]
   before_action :verify_privacy!, only: [:show, :stats_recent, :stats_rarity]
 
-  COLLECTIONS = %w(achievements mounts minions orchestrions spells beasts hairstyles emotes bardings armoires
-  outfits fashions facewear frames cards field_records survey_records occult_records).freeze
+  COLLECTIONS = %w(achievements mounts minions hairstyles emotes orchestrions frames spells beasts bardings fashions
+  facewear outfits armoires cards field_records survey_records occult_records).freeze
   STATS_COLLECTIONS = COLLECTIONS.dup.insert(1, 'titles').freeze
 
   def show
