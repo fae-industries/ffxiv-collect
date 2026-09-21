6cc58fc9a005e5e622910c74bccef370197813e8
Matt Antonelli <contact@raelys.com>
2026-09-21 22:45:10 +0000

    Update repo URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '~> 3.19.2'
 
 set :application, 'collect'
-set :repo_url,    'https://github.com/skyborn-industries/ffxiv-collect'
+set :repo_url,    'https://github.com/fae-industries/ffxiv-collect'
 set :branch,      ENV['BRANCH_NAME'] || 'main'
 set :deploy_to,   '/var/rails/collect'
 set :default_env, { path: '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
